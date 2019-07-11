e3bdce89ff5948ac88955dec0c2ea40f638208dd
Anam <anammainfolder@Anams-MacBook-Pro.local>
2019-07-11 21:21:11 +1200

    portfolio commit

---
+++ script.js
@@ -0,0 +1,59 @@
+// my teams/interests
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none"; 
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1} 
+  slides[slideIndex-1].style.display = "block"; 
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+
+// hover function
+
+// button calls
+$( ".hcGit" ).click(function() {
+  window.open ('https://github.com/erindhoxha/happyCentro' , '_blank')
+  
+});
+
+$( ".jsPro" ).click(function() {
+  window.open('http://anam.khan.yoobee.net.nz/summative/summative.htm', '_blank')
+  
+});
+
+$( ".jaanPro" ).click(function() {
+  window.open ('http://anam.khan.yoobee.net.nz/jaan/cuisine.html', '_blank')
+  
+});
+
+$( ".hcSite" ).click(function() {
+  window.open('https://erindhoxha.github.io/happyCentro/', '_blank')
+  
+});
+
+$(".linkicon").click(function() {
+  
+  window.open("https://www.linkedin.com/in/anam-khan-71ba87140","_blank")
+});
+
+// scroll to work
+
+$(".works").click(function() {
+  $('html, body').animate({
+      scrollTop: $(".myWork").offset().top
+  }, 700);
+});
+
+$(".downImg").click(function() {
+  $('html, body').animate({
+      scrollTop: $(".myWork").offset().top
+  }, 700);
+});
